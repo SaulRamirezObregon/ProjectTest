0fc6e04b0f907727e38a768d81643bd5a56782bc
saul <bychawita@gmail.com>
2016-07-17 12:28:03 -0500

    diseño

---
+++ Design/Operaciones.js
@@ -0,0 +1,31 @@
+var sumar = function( primerNumero , segundoNumero  )
+		{
+
+			var resultado = primerNumero + segundoNumero;
+			alert(resultado);
+			return resultado;
+		}
+
+		var restar = function( primerNumero , segundoNumero  )
+		{
+
+			var resultado = primerNumero - segundoNumero;
+			alert(resultado);
+			return resultado;
+		}
+
+		var dividir = function( primerNumero , segundoNumero  )
+		{
+
+			var resultado = primerNumero / segundoNumero;
+			alert(resultado);
+			return resultado;
+		}
+
+		var multiplicar = function( primerNumero , segundoNumero  )
+		{
+
+			var resultado = primerNumero * segundoNumero;
+			alert(resultado);
+			return resultado;
+		}
